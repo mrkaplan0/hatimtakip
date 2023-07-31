51096b483d2abe9c4b0bb2e415b7b5673bfe94b5
mrkaplan0 <kaplan5019@gmail.com>
2023-07-31 23:26:42 +0200

    Initial Commit

---
+++ hatimtakipapp/hatimtakipappApp.swift
@@ -0,0 +1,17 @@
+//
+//  hatimtakipappApp.swift
+//  hatimtakipapp
+//
+//  Created by MrKaplan on 31.07.23.
+//
+
+import SwiftUI
+
+@main
+struct hatimtakipappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

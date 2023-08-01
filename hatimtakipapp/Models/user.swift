2cacc2ed654e6ccb2c8927b56cafbe4ef1d1694d
mrkaplan0 <kaplan5019@gmail.com>
2023-08-02 00:11:37 +0200

    create new group and add user UI etc.

---
+++ hatimtakipapp/Models/user.swift
@@ -0,0 +1,19 @@
+//
+//  user.swift
+//  hatimtakipapp
+//
+//  Created by MrKaplan on 01.08.23.
+//
+
+import Foundation
+
+class user {
+    
+    let id : String = UUID().uuidString
+    let email : String = ""
+    let username : String = ""
+    
+    init() {
+    }
+    
+}
--- hatimtakipapp/Views/AddUserToGroupPage.swift
+++ hatimtakipapp/Views/AddUserToGroupPage.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct AddUserToGroupPage: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        let navigationTitle = "Kisi Ekle"
+        let searchFieldInfoText = "Kisi bul ve ekle"
+        let names = ["Holly", "Josh", "Rhonda", "Ted","Hasan"]
+           @State private var searchText = ""
+
+           var body: some View {
+               NavigationStack {
+                   
+                   // search user, click and add
+                   
+                   List {
+                       ForEach(searchResults, id: \.self) { name in
+                           Text(name).onTapGesture {
+                               print("Hallo")
+                           }
+                       }
+                   }
+                   .navigationTitle("\(navigationTitle)")
+               }
+               .searchable(text: $searchText, prompt: Text("\(searchFieldInfoText)"))
+           }
+
+           var searchResults: [String] {
+               if searchText.isEmpty {
+                   return []
+               } else {
+                   return names.filter { $0.contains(searchText) }
+               }
+           }
+    
+  
+       }
+
 
 struct AddUserToGroupPage_Previews: PreviewProvider {
     static var previews: some View {
